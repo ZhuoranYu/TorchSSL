2e633109d696243e43158760b11f8f9231b1e17f
ZhuoranYu <zhuoranyu@gatech.edu>
2022-03-24 02:53:47 -0400

    0.95 bin

--- analyze_utils.py
+++ analyze_utils.py
@@ -3,19 +3,26 @@ import torch
 
 def analyze_prob(scores, predictions, labels):
     res = {}
-    for t in np.arange(0.5, 1.0, 0.1):
-        mask = torch.logical_and(scores >= t, scores < t + 0.1)
+    bins = [0.5, 0.6, 0.7, 0.8, 0.9, 0.95]
+    for idx, t in enumerate(bins):
+        if t < 0.9:
+            t = np.round(t, 2)
+            t1 = np.round(t + 0.1, 2)
+        else:
+            t = np.round(t, 2)
+            t1 = np.round(t + 0.05, 2)
+        mask = torch.logical_and(scores >= t, scores < t1)
         score_bin = scores[mask]
 
         if torch.numel(score_bin) == 0:
-            res[f'precision/{np.round(t, 1)}-to-{np.round(t+0.1, 1)}'] = 0
-            res[f'count/{np.round(t, 1)}-to-{np.round(t+0.1, 1)}'] = 0
+            res[f'precision/{t}-to-{t1}'] = 0
+            res[f'count/{t}-to-{t1}'] = 0
             continue
         pred_bin = predictions[mask]
         labels_bin = labels[mask]
         tp_bin = pred_bin[pred_bin == labels_bin]
-        res[f'precision/{np.round(t, 1)}-to-{np.round(t + 0.1, 1)}'] = torch.numel(tp_bin) / torch.numel(pred_bin)
-        res[f'count/{np.round(t, 1)}-to-{np.round(t + 0.1, 1)}'] = pred_bin.shape[0]
+        res[f'precision/{t}-to-{t1}'] = torch.numel(tp_bin) / torch.numel(pred_bin)
+        res[f'count/{t}-to-{t1}'] = pred_bin.shape[0]
 
     return res
 
